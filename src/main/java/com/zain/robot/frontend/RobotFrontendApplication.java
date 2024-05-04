ceba2ef6f794e2803e58cd26eac31a9eea70354b
Zain I <zain.imtiaz93@gmail.com>
2024-05-04 16:36:16 +0400

    Created initial project structure for backend development of the Robot Assignment

---
+++ src/main/java/com/zain/robot/frontend/RobotFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.zain.robot.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotFrontendApplication.class, args);
+	}
+
+}
