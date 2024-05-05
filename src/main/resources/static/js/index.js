89f5c3e594aad444695656fd3a1a82d038a50147
Zain I <zain.imtiaz93@gmail.com>
2024-05-05 22:43:56 +0400

    Updated index page and removed unused code. Also updated the logic to display the robot. Included js file and added javascript logic in the file to render the robot image as per the executed command.

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,59 @@
+
+function moveRobot() {
+    // Get current row and column index
+    var currentRowIndex = rowPosition - 1;
+    var currentColIndex = colPosition - 1;
+
+    if (executionCommand != null) {
+        clearPreviousRobotLocation();
+
+        var currentCell = $('#item-cell-' + currentRowIndex + '-' + currentColIndex);
+        currentCell.addClass("robot");
+
+        var cellClass = "";
+        if (facePosition == "UP") {
+            cellClass = "robot-up";
+        } else if (facePosition == "RIGHT") {
+            cellClass = "robot-left";
+        } else if (facePosition == "LEFT") {
+            cellClass = "robot-right";
+        }
+        currentCell.addClass(cellClass);
+
+        $('#currentState').text(executionCommand.operationType);
+        if (executionCommand != null) console.log("commandResponse: " + JSON.stringify(executionCommand));
+
+        var commandScriptIndexNumberVal = parseInt($('#commandScriptIndexNumber').val());
+        if (commandScriptIndexNumberVal < 6) {
+            $('#commandScriptIndexNumber').val(commandScriptIndexNumberVal+1);
+            $('#rowPosition').val(rowPosition);
+            $('#colPosition').val(colPosition);
+            $('#facePosition').val(facePosition);
+
+            $('#commandForm').delay(5000).submit();
+        }
+
+    }
+}
+
+function clearPreviousRobotLocation() {
+    // Remove robot icon from all cells
+    ['.robot', '.robot-up', '.robot-left', '.robot-right'].forEach(
+        function(className) {
+            $(className).remove();
+        });
+}
+
+function setInitialValues() {
+    // Set initial values for robot's position
+    $('#commandScriptIndexNumber').val(0);
+    $('#rowPosition').val(1);
+    $('#colPosition').val(1);
+    $('#facePosition').val("DOWN");
+}
+
+
+$(document).ready(function() {
+    moveRobot();
+});
+
