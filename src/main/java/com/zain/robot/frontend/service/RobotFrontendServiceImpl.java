1fa17fe70de7ec34b93169aca33766354f2dd7e9
Zain I <zain.imtiaz93@gmail.com>
2024-05-05 21:51:38 +0400

    Updated Frontend Controller for simplicity and updated the endpoint methods to accommodate the logic change. Updated the service to deduce the face position from the command being executed.

--- src/main/java/com/zain/robot/frontend/service/RobotFrontendServiceImpl.java
+++ src/main/java/com/zain/robot/frontend/service/RobotFrontendServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
+import static com.zain.robot.frontend.util.constant.RobotFrontendConstant.*;
+
 @Service
 @Slf4j
 @AllArgsConstructor
@@ -16,13 +18,49 @@ public class RobotFrontendServiceImpl implements RobotFrontendService {
 
     private final RobotBackendIntegration robotBackendIntegration;
 
-    public CommandResponseDTO executeCommand(String command, String rowPosition, String colPosition, String facePosition) throws IOException {
+    public CommandResponseDTO executeCommand(String commandScriptIndexNumber, String rowPosition, String colPosition, String facePosition) throws IOException {
         log.info("Entered executePredefinedCommand method to call the backend API and execute commands");
-        CommandResponseDTO commandResponseDTOs = robotBackendIntegration.fetchCommandResponse(
-                RobotFrontendConstant.ROBOT_BACKEND_EXECUTE_COMMAND_URL,
-                command, rowPosition, colPosition, facePosition);
+        String command = RobotFrontendConstant.PRE_DEFINED_COMMANDS.get(Integer.parseInt(commandScriptIndexNumber));
+        facePosition = deduceFacePosition(facePosition, command);
 
-        log.info("Response returned from backend API to execute commands: {}", commandResponseDTOs);
-        return commandResponseDTOs;
+        CommandResponseDTO commandResponseDTO = robotBackendIntegration.fetchCommandResponse(
+                ROBOT_BACKEND_EXECUTE_COMMAND_URL, command, rowPosition, colPosition, facePosition);
+
+        commandResponseDTO.setFacePosition(facePosition);
+        log.info("Response returned from backend API to execute commands: {}", commandResponseDTO);
+        return commandResponseDTO;
+    }
+
+    private String deduceFacePosition(String facePosition, String command) {
+        String[] subCommands = command.split(" ");
+        switch (subCommands[0]) {
+            case POSITION:
+                return FACE_DOWN;
+            case TURNAROUND:
+                switch (facePosition) {
+                    case FACE_LEFT:
+                        return FACE_RIGHT;
+                    case FACE_RIGHT:
+                        return FACE_LEFT;
+                    case FACE_UP:
+                        return FACE_DOWN;
+                    case FACE_DOWN:
+                        return FACE_UP;
+                }
+                break;
+            case RIGHT:
+                switch (facePosition) {
+                    case FACE_LEFT:
+                        return FACE_UP;
+                    case FACE_RIGHT:
+                        return FACE_DOWN;
+                    case FACE_UP:
+                        return FACE_RIGHT;
+                    case FACE_DOWN:
+                        return FACE_LEFT;
+                }
+                break;
+        }
+        return facePosition;
     }
 }
