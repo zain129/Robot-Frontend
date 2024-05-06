f3f025cc8be9a900b94be77758b4c7c5f6724608
Zain I <zain.imtiaz93@gmail.com>
2024-05-07 00:33:49 +0400

    Added Bean for WebClient and removed RestTemplate. Consumed backend APIs using web client for both custom commands and trial command methods

--- src/main/java/com/zain/robot/frontend/RobotFrontendApplication.java
+++ src/main/java/com/zain/robot/frontend/RobotFrontendApplication.java
@@ -1,20 +1,29 @@
 package com.zain.robot.frontend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class RobotFrontendApplication {
+
+    @Value("${robot.backend.root_url}")
+    private String robotBackendRootUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(RobotFrontendApplication.class, args);
     }
 
-    @Bean("genericRestTemplate")
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+    @Bean("robotBackendWebClient")
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(robotBackendRootUrl)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 
 }
--- src/main/java/com/zain/robot/frontend/integration/RobotBackendIntegration.java
+++ src/main/java/com/zain/robot/frontend/integration/RobotBackendIntegration.java
@@ -1,53 +1,44 @@
 package com.zain.robot.frontend.integration;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.zain.robot.frontend.domain.dto.CommandRequestDTO;
 import com.zain.robot.frontend.domain.dto.CommandResponseDTO;
-import com.zain.robot.frontend.exception.NoResponseFoundException;
-import com.zain.robot.frontend.util.CommonRestUtil;
+import com.zain.robot.frontend.util.constant.RobotFrontendConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class RobotBackendIntegration {
 
-    private final RestTemplate restTemplate;
-    private final String robotBackendRootUrl;
+    private final WebClient webClient;
 
-    public RobotBackendIntegration(@Qualifier("genericRestTemplate") RestTemplate restTemplate,
-                                   @Value("${robot.backend.root_url}") String robotBackendRootUrl) {
-        this.restTemplate = restTemplate;
-        this.robotBackendRootUrl = robotBackendRootUrl;
+    public RobotBackendIntegration(@Qualifier("robotBackendWebClient") WebClient webClient) {
+        this.webClient = webClient;
     }
 
-    public CommandResponseDTO fetchCommandResponse(String endpoint, String command, String rowPosition
-            , String colPosition, String facePosition) throws JsonProcessingException {
-        String uri = robotBackendRootUrl + endpoint;
-        log.info("Entered fetchCommandResponse method to call the endpoint: {}", endpoint);
+    public ResponseEntity<CommandResponseDTO> fetchCommandResponse(CommandRequestDTO commandRequestDTO) {
+        return webClient.post()
+                .uri(RobotFrontendConstant.ROBOT_BACKEND_EXECUTE_COMMAND_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(commandRequestDTO)
+                .retrieve()
+                .toEntity(CommandResponseDTO.class)
+                .block();
+    }
 
-        CommandRequestDTO commandRequestDTO = CommandRequestDTO.builder()
-                .stringCommand(command)
-                .currentRowPosition(Long.valueOf(rowPosition))
-                .currentColPosition(Long.valueOf(colPosition))
-                .facePosition(facePosition)
-                .build();
-
-        ResponseEntity<CommandResponseDTO> responseEntity = restTemplate.exchange(uri, HttpMethod.POST,
-                CommonRestUtil.buildHttpEntityRequest(commandRequestDTO, MediaType.APPLICATION_JSON), CommandResponseDTO.class);
-
-        if (responseEntity.hasBody()) {
-            log.info("Endpoint {} returned response {}", endpoint, responseEntity);
-            return responseEntity.getBody();
-        } else {
-            log.error("No or empty response received by Endpoint {}", endpoint);
-            throw new NoResponseFoundException("No response found from backend for the provided commands list");
-        }
+    public ResponseEntity<List<CommandResponseDTO>> fetchListOfCommandResponses(List<CommandRequestDTO> commandRequestDTO) {
+        return webClient.post()
+                .uri(RobotFrontendConstant.ROBOT_BACKEND_EXECUTE_ALL_COMMAND_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(commandRequestDTO)
+                .retrieve()
+                .toEntityList(CommandResponseDTO.class)
+                .block();
     }
 }
