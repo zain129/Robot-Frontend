e22641c15026e4b00fe3bd3a32da88b3dca4925d
Zain I <zain.imtiaz93@gmail.com>
2024-05-07 00:53:47 +0400

    Included test cases for controller, service, and integration to ensure testing of the system

--- src/test/java/com/zain/robot/frontend/controller/RobotFrontendControllerTest.java
+++ src/test/java/com/zain/robot/frontend/controller/RobotFrontendControllerTest.java
@@ -9,7 +9,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.aot.DisabledInAotMode;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
